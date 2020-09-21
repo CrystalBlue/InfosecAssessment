b927f34c164d044adc7b6f133a341a458b8ce47e
Josh Schultz <crystalblue.josh@gmail.com>
2020-09-21 15:48:28 -0500

    Changes to how props are accessed

--- testContries/js/app.js
+++ testContries/js/app.js
@@ -1,4 +1,11 @@
 class CountryContainer extends React.Component {
+	countryData = [];
+	
+	constructor(props) {
+		super(props);
+		this.state = { countryData: []};
+	}
+	
 	onFormSubmitSuccess(e) {
 		e.preventDefault();
 		$.ajax({
@@ -7,9 +14,8 @@ class CountryContainer extends React.Component {
 			data: {searchQ: $("#searchInput").val()},
 			dataType: "json",
 			success: function(data) {
-				console.log(data);
+				this.countryData = data;
 				this.setState({"countryData":data});
-				console.log(this.state.countryData);
 			}.bind(this),
 			error: function(xhr, status,err) {
 				console.log('error')
@@ -26,7 +32,7 @@ class CountryContainer extends React.Component {
 					<button onClick={this.onFormSubmitSuccess.bind(this)} >Search</button>
 				</div>
 				<br />
-				<CountryTable />
+				<CountryTable data={this.countryData} />
 			</div>
 		)
 	}
@@ -34,15 +40,12 @@ class CountryContainer extends React.Component {
 
 class CountryTable extends React.Component {
 	constructor(props) {
-		super(props)
-		this.state = {
-			countryData: []
-		}
+		super(props);
 	}
 	
 	renderTableData() {
 		console.log("Render Table Data");
-		return this.state.countryData.map(country => {
+		return this.props.data.map(country => {
 			return (
 				<tr key={country.alpha3Code}>
 					<td>{country.name}</td>
@@ -52,7 +55,6 @@ class CountryTable extends React.Component {
 					<td>{country.region}</td>
 					<td>{country.subregion}</td>
 					<td>{country.population}</td>
-					<td>{country.languages}</td>
 				</tr>
 			)
 		});
@@ -60,27 +62,32 @@ class CountryTable extends React.Component {
 	
 	render() {
 		console.log("Render Table");
-		console.log(this.state.countryData);
-		return (
-			<div className="countryTab">
-				<h3>Country List</h3>
-				<table border="1" id="countries" className="countryTable">
-					<tbody>
-						<tr>
-							<th>Country Name</th>
-							<th>Country Alpha 2 Code</th>
-							<th>Country Alpha 3 Code</th>
-							<th>Flag Image</th>
-							<th>Region</th>
-							<th>Sub Region</th>
-							<th>Population</th>
-							<th>Languages</th>
-						</tr>
-						{this.renderTableData()}
-					</tbody>
-				</table>
-			</div>
-		);
+		console.log(this.props.data);
+		if (this.props === undefined || this.props.length == 0) {
+			return (
+				<div>There is no country data to show.</div>
+			)
+		} else {
+			return (
+				<div className="countryTab">
+					<h3>Country List</h3>
+					<table border="1" id="countries" className="countryTable">
+						<tbody>
+							<tr>
+								<th>Country Name</th>
+								<th>Country Alpha 2 Code</th>
+								<th>Country Alpha 3 Code</th>
+								<th>Flag Image</th>
+								<th>Region</th>
+								<th>Sub Region</th>
+								<th>Population</th>
+							</tr>
+							{this.renderTableData()}
+						</tbody>
+					</table>
+				</div>
+			);
+		}
 	}
 }
 
